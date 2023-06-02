1ec4872febcfcf5132c68fe898e019ad08d86e84
lichang <2798323033>
2023-06-02 15:24:44 +0800

    添加动态类名

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/style/reset.css'
-import '@/assets/style/index.css'
+import '@/assets/style/index.less'
 
 import App from './App.vue'
 import router from './router'
