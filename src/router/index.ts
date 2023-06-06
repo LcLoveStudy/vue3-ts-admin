83baa146a0dc2baba90f38303a0554f341bc4761
lichang <2798323033>
2023-06-06 16:54:47 +0800

    解决打包空白问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HOME from './modules/home'
 import SETTINGS from './modules/settings'
 import type { interRouter } from '@/type/route.type'
@@ -30,7 +30,7 @@ const initRoute = (routeArr: Array<interRouter>) => {
 initRoute(routes)
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
