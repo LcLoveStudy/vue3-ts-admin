aaea8be51194c6b3dc217e196e963aa29c2b3f67
lichang <2798323033>
2023-06-09 11:00:15 +0800

    修改focus

--- src/directive/v-focus.ts
+++ src/directive/v-focus.ts
@@ -1,49 +1,56 @@
+import { nextTick } from 'vue'
 export const focus = {
   name: 'focus',
   method: {
-    mounted(el: HTMLElement) {
+    mounted(el: HTMLElement, value: any) {
+      //是否是element的input组件，如果是则获取input框并获取焦点
+      if (
+        el.getAttribute('class') &&
+        el.getAttribute('class')?.split(' ').indexOf('el-input') !== -1 &&
+        value.value
+      ) {
+        //vue3+element-plus写法
+        if (el.getElementsByTagName('div')[0]) {
+          el.getElementsByTagName('div')[0].getElementsByTagName('input')[0].focus()
+        }
+      } else {
+        if (value.value) {
+          //设置tabindex属性,让无法聚焦的元素获取聚焦功能
+          el.setAttribute('tabindex', Math.floor(Math.random() * 10 + 1).toString())
+          el.focus()
+        }
+      }
+    },
+    beforeUpdate(el: HTMLElement, value: any) {
       //是否是element的input组件，如果是则获取input框并获取焦点
       if (
         el.getAttribute('class') &&
         el.getAttribute('class')?.split(' ').indexOf('el-input') !== -1
       ) {
-        //vue3+element-plus写法
-        if (el.getElementsByTagName('div')[0]) {
-          el.getElementsByTagName('div')[0]
-            .getElementsByTagName('input')[0]
-            .focus()
+        if (value.value) {
+          nextTick(() => {
+            //vue3+element-plus写法
+            el.getElementsByTagName('div')[0].getElementsByTagName('input')[0].focus()
+          })
+        } else {
+          nextTick(() => {
+            //vue3+element-plus写法
+            el.getElementsByTagName('div')[0].getElementsByTagName('input')[0].blur()
+          })
         }
       } else {
-        //设置tabindex属性,让无法聚焦的元素获取聚焦功能
-        el.setAttribute(
-          'tabindex',
-          Math.floor(Math.random() * 10 + 1).toString()
-        )
-        el.focus()
+        if (value.value) {
+          nextTick(() => {
+            //设置tabindex属性,让无法聚焦的元素获取聚焦功能
+            el.setAttribute('tabindex', Math.floor(Math.random() * 10 + 1).toString())
+            el.focus()
+          })
+        } else {
+          nextTick(() => {
+            el.blur()
+          })
+        }
       }
-    },
-  },
+    }
+  }
 }
-
-// vue2+element-ui写法
-// //自动聚焦指令
-// Vue.directive('focus', {
-//   inserted: function (el) {
-//     //是否是element的input组件，如果是则获取input框并获取焦点
-//     if (el.getAttribute('class').split(' ').indexOf('el-input') !== -1) {
-//       //vue3+element-plus写法
-//       if (el.getElementsByTagName('div')[0]) {
-//         el.getElementsByTagName('div')[0]
-//           .getElementsByTagName('input')[0]
-//           .focus()
-//       } else {
-//         //vue2+element-ui写法
-//         el.getElementsByTagName('input')[0].focus()
-//       }
-//     } else {
-//       //设置tabindex属性,让无法聚焦的元素获取聚焦功能
-//       el.setAttribute('tabindex', Math.floor(Math.random() * 10 + 1))
-//       el.focus()
-//     }
-//   }
-// })
--- src/views/settings/holle.vue
+++ src/views/settings/holle.vue
@@ -1,9 +1,11 @@
 <template>
-  hello
+  <el-input v-focus="focused"></el-input>
+  <el-button type="primary" @click="focused = !focused">点我改变聚焦状态</el-button>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue'
+const focused = ref(false)
 </script>
 
 <style scoped></style>
