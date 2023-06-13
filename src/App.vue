f926b1f338d02589d2704c3ab7158807fc97a34a
lichang <2798323033>
2023-06-13 13:35:58 +0800

    add common.css to assets

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { getList } from '@/api'
-const getMylist = () => {
-  getList()
-}
-getMylist()
 </script>
 
 <style scoped lang="less"></style>
