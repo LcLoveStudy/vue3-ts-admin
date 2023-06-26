0ecac66db99947394eb51b6e1e4d6c2a1290504c
lichang <2798323033>
2023-06-26 11:34:54 +0800

    删除无用指令

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,8 +1,7 @@
 import { focus } from './v-focus'
 import { lazy } from './v-lazy'
-import { Ownloading } from './loading/v-loading'
 import { clickoutside } from './v-clickoutside'
-export const directives = [focus, Ownloading, lazy, clickoutside]
+export const directives = [focus, lazy, clickoutside]
 
 // main.ts中批量导入自定义指令
 // directives.forEach((item) => {
