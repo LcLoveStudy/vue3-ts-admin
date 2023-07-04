4ed078bbf26fbb5e20c897c309e612083de512d6
lichang <2798323033>
2023-07-04 14:10:07 +0800

    fix:解决引入组件报错的问题

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 declare module '*.vue' {
+  //解决引入组件报错的问题
   import { ComponentOptions } from 'vue'
   const componentOptions: ComponentOptions
   export default componentOptions
