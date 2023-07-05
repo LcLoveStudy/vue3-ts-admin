64bbfcdd45fdd8a40d4ad21ef6da18d2541e0b5d
lichang <2798323033>
2023-07-05 17:33:08 +0800

    实现了vditor编辑器离线

---
+++ src/components/richEditor/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="vditor" ></div>
+</template>
+
+<script setup lang="ts">
+import Vditor from "./src"
+import 'vditor/dist/js/icons/ant'
+import "vditor/dist/index.css"
+import {onMounted,ref} from 'vue'
+const props = defineProps({
+    value: {
+      type: String,
+      default: ''
+    },
+    isHideTools: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    isPin: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    height: {
+      type: String,
+      required: false,
+      default: '300px'
+    },
+    width: {
+      type: String,
+      required: false,
+      default: "auto"
+    },
+    mode: {
+      type: String,
+      required: false,
+      default: 'sv'
+    },
+  })
+const vditor = ref()
+  onMounted(()=>{
+    vditor.value = new Vditor("vditor", {
+      height: props.height,
+      width: props.width,
+      toolbarConfig: {
+        pin: false,
+        hide:false,
+      },
+      cache: {
+        enable: false
+      },
+      toolbar: [
+        // "emoji",
+        // "headings",
+        "bold",
+        "italic",
+        "strike",
+        "link",
+        "|",
+        "list",
+        "ordered-list",
+        "check",
+        "outdent",
+        "indent",
+        "|",
+        "quote",
+        "line",
+        "code",
+        "inline-code",
+        "insert-before",
+        "insert-after",
+        "|",
+        //"upload",
+        // "record",
+        { //自定义上传
+          hotkey: "",
+          name: "upload",
+          // tipPosition: "s",
+          tip: "上传图片",
+          className: "right",
+        },
+ 
+        "table",
+        "|",
+        "undo",
+        "redo",
+        "|",
+        // "fullscreen",
+        // "edit-mode",
+        {
+          name: "more",
+          toolbar: [
+            //"both",
+            // "code-theme",
+            // "content-theme",
+            // "export",
+            "outline",
+            "preview",
+            //"devtools",
+            // "info",
+            //"help",
+          ],
+        },
+      ],
+      after: () => {
+        vditor.value.setValue(props.value)
+      },
+      mode: 'wysiwyg',
+      preview: {
+        mode: "both",
+        actions: []
+      },
+    })
+  })
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,10 +2,12 @@
   <div class="mb-20">
     home
   </div>
+  <RichEditor/>
 </template>
 
 <script setup lang="ts">
 import { getList } from '@/api'
+import RichEditor from '@/components/richEditor/index.vue'
 getList({ page: 1 })
 </script>
 
