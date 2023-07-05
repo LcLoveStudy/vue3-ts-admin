21c0d84ef67de164564bd7001984b477a7ad4074
lichang <2798323033>
2023-07-05 14:10:59 +0800

    调整utils文件夹结构

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,21 @@
-import { getItem, setItem, removeItem } from './localstorage'
+import { getItem, setItem, removeItem } from './common/localstorage'
 import {startLoading,endLoading }from './page/nprogress'
 import { arrNoRepeat, arrSort, isArray } from './array'
 import { deepFreeze, objectCopy } from './object'
 import { pageScroll } from './page/pageScroll'
 import { Percentage } from './page/percentage'
 import { dateFormat } from './dateFormat'
-import { downLoad, downloadBlob } from './download'
+import { downLoad } from './file/download'
 import getColor from './getColor'
 import getType from './getType'
-import hasRole from './hasRole'
-import getCopy from './copy'
+import hasRole from './auth/hasRole'
+import getCopy from './common/copy'
 import http from './http'
 export {
   Percentage,
   http,
   downLoad,
   arrNoRepeat,
-  downloadBlob,
   startLoading,
   endLoading,
   arrSort,
