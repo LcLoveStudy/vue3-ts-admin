e398e5821c4a134ff8a92ec0bfeb204b2ae52ab0
lichang <2798323033>
2023-07-06 16:36:53 +0800

    commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="mb-20">
+  <div class="mb-20 fs-20 fw-700">
     home
   </div>
-  <RichEditor v-model:value="inputValue" @change="inputChange"/>
+  <RichEditor v-model:value="inputValue" @change="inputChange" />
 </template>
 
 <script setup lang="ts">
 import { getList } from '@/api'
 import RichEditor from '@/components/richEditor/index.vue'
-import {ref} from 'vue'
+import { ref } from 'vue'
 const inputValue = ref('')
-const inputChange = ()=>{
+const inputChange = () => {
   console.log(inputValue.value)
 }
 getList({ page: 1 })
