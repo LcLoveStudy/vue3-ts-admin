e64274f8d84e3eb6e7638d66794ee1da7b895b85
lichang <2798323033>
2023-08-07 15:29:18 +0800

    修改路径符

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { createPinia } from 'pinia'
 import '@/utils/page/rem.js' // 引入rem解决方案
 import '@/assets/style/reset.css' // 样式重置
 import '@/assets/style/atomization.css' // 引入原子化css
-import useElPlus from './components/useElPlus'
+import useElPlus from '@/components/useElPlus'
 import createDirective from '@/directive'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 const app = createApp(App) // 实例化
 createDirective(app) // main.ts中批量导入自定义指令
 useElPlus(app) // 注册elementPlus
