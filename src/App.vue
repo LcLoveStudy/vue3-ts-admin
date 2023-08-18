8d3818ede488a0df2de3da596cadcb436e2867ad
lichang <2798323033>
2023-08-18 12:30:57 +0800

    change:修改main.ts引入

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,3 @@
 </template>
 
 <script setup lang="ts"></script>
-
-<style lang="less">
-  @import 'nprogress/nprogress.css';
-  @import 'element-plus/dist/index.css';
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
+// vue core
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import '@/utils/page/rem.js' // 引入rem解决方案
-import '@/assets/style/reset.css'
-import '@/assets/style/atomization.css' // 引入原子化css
-import useElPlus from '@/components/useElPlus'
-import createDirective from '@/directive'
 import App from '@/App.vue'
 import router from '@/router'
+// init
+import useElPlus from '@/components/useElPlus'
+import createDirective from '@/directive'
+// css
+import '@/utils/page/rem.js'
+import '@/assets/style/reset.css'
+import '@/assets/style/atomization.css'
+import 'nprogress/nprogress.css'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
+/** 自定义指令 */
 createDirective(app)
+/** 挂载element-plus */
 useElPlus(app)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router)
 app.mount('#app')
