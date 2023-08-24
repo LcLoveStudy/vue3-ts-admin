7a2af2a7025f98cbfc2343c19530862c61493dad
lichang <2798323033>
2023-08-24 10:40:07 +0800

    del:删除多余函数

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,49 +1,4 @@
 /**
- * 时间格式化
- * @param {*} date 初始时间(Fri Apr 07 2023 13:50:24 GMT+0800 (中国标准时间))
- * @returns 转换为YYYY年MM月DD h:m:s格式的时间字符串
- * @example dateFormat(new Date())
- * @author 李畅
- * @email dotb116393@163.com
- */
-export const dateFormat = (date: Date) => {
-  const newDate = new Date(date)
-  const result = 'YYYY年MM月DD h:m:s'
-    .replace('YYYY', newDate.getFullYear().toString())
-    .replace(
-      'MM',
-      (newDate.getMonth() + 1).toString().length === 1
-        ? 0 + (newDate.getMonth() + 1).toString()
-        : (newDate.getMonth() + 1).toString()
-    )
-    .replace(
-      'DD',
-      newDate.getDate().toString().length === 1
-        ? 0 + newDate.getDate().toString()
-        : newDate.getDate().toString()
-    )
-    .replace(
-      'h',
-      newDate.getHours().toString().length === 1
-        ? 0 + newDate.getHours().toString()
-        : newDate.getHours().toString()
-    )
-    .replace(
-      'm',
-      newDate.getMinutes().toString().length === 1
-        ? 0 + newDate.getMinutes().toString()
-        : newDate.getMinutes().toString()
-    )
-    .replace(
-      's',
-      newDate.getSeconds().toString().length === 1
-        ? 0 + newDate.getSeconds().toString()
-        : newDate.getSeconds().toString()
-    )
-  return result
-}
-
-/**
  * 将秒数转换时分秒
  * @param timeStamp 秒数
  * @returns 时分秒的格式
--- src/views/documents/utils/components/DateUse.vue
+++ src/views/documents/utils/components/DateUse.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="flex flex-wrap ml-20">
-    <!-- 格式化时间对象 -->
-    <el-card class="width-400 height-full">
-      <div class="fs-16 fw-600 tomato">
-        格式化时间对象(dateFormat)
-        <el-icon class="hover-pointer" @click="copyUtils('dateFormat')">
-          <DocumentCopy />
-        </el-icon>
-      </div>
-      <div class="mt-10">对标准时间对象进行格式化</div>
-      <div class="mt-10 pl-5 pt-5 pr-5 pb-5">
-        <span class="orange">dateFormat</span>
-        <span class="blue">(new Date())</span>
-        -> 2001年11月12日 11:12:11
-      </div>
-    </el-card>
     <!-- 将秒数转换时分秒。 -->
     <el-card class="width-400 height-full ml-20">
       <div class="fs-16 fw-600 tomato">
