61de163e0c9544b101655a7cc5e908862b5172fd
lichang <2798323033>
2023-08-24 11:05:17 +0800

    add:添加了去vueuse的链接

--- src/views/documents/utils/index.vue
+++ src/views/documents/utils/index.vue
@@ -1,4 +1,12 @@
 <template>
+  <el-card class="mb-20 fs-25">
+    <span class="tomato">vueuse:一款非常好用官方工具库</span>
+    <br />
+    <div class="flex items-center mt-10 fs-16">
+      更多配置查看
+      <el-link type="primary" @click="goVueUse"><span class="fs-16">官网</span></el-link>
+    </div>
+  </el-card>
   <el-tabs tab-position="left">
     <el-tab-pane label="通用">
       <BaseUse />
@@ -24,6 +32,9 @@
   import arrayUse from './components/ArrayUse.vue'
   import stringUse from './components/StringUse.vue'
   import DateUse from './components/DateUse.vue'
+  const goVueUse = () => {
+    window.open('https://vueuse.org/')
+  }
 </script>
 
 <style scoped lang="less"></style>
