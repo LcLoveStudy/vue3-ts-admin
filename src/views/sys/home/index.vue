6b1a3292dce2875272ac0c847d2679fe053f86e9
lichang <2798323033>
2023-10-24 16:05:54 +0800

    add:测试tailwindcss

--- src/views/sys/home/index.vue
+++ src/views/sys/home/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="mb-20 fs-20 fw-700">home</div>
+  <div class="w-[12.5rem] leading-10 pl-5 line-clamp-10 text-lg bg-lime-500">首页</div>
+  <div class="mt-2.5 pl-5">测试一下tailwindcss</div>
 </template>
 
 <script setup lang="ts">
