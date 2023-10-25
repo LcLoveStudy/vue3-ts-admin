0c7cea2f9f0393eab8b00b87b8a089f351f29c9b
lichang <2798323033>
2023-10-25 11:04:28 +0800

    update:更新404页面

--- src/views/sys/error-page/404.vue
+++ src/views/sys/error-page/404.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>页面未找到</div>
-  <el-button type="primary" @click="router.push('/')">回到主页</el-button>
+  <div class="flex flex-col items-center mt-20">
+    <div class="mb-10">抱歉，您访问的页面不存在。</div>
+    <el-button type="primary" class="w-[10rem]" @click="router.push('/')">回到主页</el-button>
+  </div>
 </template>
 
 <script setup lang="ts">
