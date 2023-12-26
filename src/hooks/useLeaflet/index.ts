32452062ff82ce3ed35ed6879b15bcc82ace78f1
lichang <2798323033>
2023-12-26 13:50:11 +0800

    add:添加leaflethook

--- src/hooks/useLeaflet/index.ts
+++ src/hooks/useLeaflet/index.ts
@@ -7,11 +7,19 @@ import 'leaflet/dist/leaflet.css'
  * @param mapId 地图dom的id
  * @param url 瓦片图的url
  * @param center 初始时的中心点
- * @returns mapRef 地图实例
+ * @returns
+ * mapRef 地图实例
+ * currentLnglat 鼠标当前所在位置
  */
 export const useLeaflet = (mapId: string, url: string, center: LatLngExpression) => {
   const mapRef = ref()
+  const currentLnglat = ref({
+    lng: 0,
+    lat: 0
+  })
+  // 视图范围
   const bounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180))
+  // 地图配置
   const osm = L.tileLayer(url, {
     maxNativeZoom: 15,
     maxZoom: 15,
@@ -28,6 +36,10 @@ export const useLeaflet = (mapId: string, url: string, center: LatLngExpression)
       layers: [osm],
       maxBounds: bounds
     })
+    mapRef.value.on('mousemove', function (e: any) {
+      currentLnglat.value.lat = e.latlng.lat.toFixed(5)
+      currentLnglat.value.lng = e.latlng.lng.toFixed(5)
+    })
   })
-  return { mapRef }
+  return { mapRef, currentLnglat }
 }
