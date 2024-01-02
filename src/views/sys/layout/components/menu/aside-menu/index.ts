db3b9061c1e2fefea809066718d59896eb73a2c8
lichang <2798323033>
2024-01-02 12:03:58 +0800

    update:提取aside-menu的样式

---
+++ src/views/sys/layout/components/menu/aside-menu/index.ts
@@ -0,0 +1,4 @@
+import './src/index.less'
+import AsideMenu from './src/index.vue'
+
+export default AsideMenu
--- src/views/sys/layout/index.vue
+++ src/views/sys/layout/index.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script setup lang="ts">
-  import AsideMenu from './components/menu/aside-menu.vue'
+  import AsideMenu from './components/menu/aside-menu'
   import Header from './components/header/index.vue'
   import { useLayoutStore } from '@/stores/modules/layout'
   import { storeToRefs } from 'pinia'
