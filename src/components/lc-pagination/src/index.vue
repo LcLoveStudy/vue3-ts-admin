dc4006b22b558fa2a5a64c92399bc6d66b5756c4
lichang <2798323033>
2024-05-08 10:06:02 +0800

    update:使用defineModel优化代码

--- src/components/lc-pagination/src/index.vue
+++ src/components/lc-pagination/src/index.vue
@@ -5,58 +5,36 @@
     background
     :layout="props.layout"
     :total="props.total"
-    @size-change="currentChange"
-    @current-change="currentChange"
+    @size-change="sizeChangeHandler"
+    @current-change="emits('change')"
   />
 </template>
 
 <script setup lang="ts">
   defineOptions({
-    name: 'SsPagination'
+    name: 'LcPagination'
+  })
+  const size = defineModel('size', {
+    type: Number,
+    default: 10
+  })
+  const current = defineModel('current', {
+    type: Number,
+    default: 1
   })
   const props = defineProps({
-    current: {
-      type: Number,
-      default: 1
-    },
     layout: {
       type: String,
       default: 'total, sizes, prev, pager, next, jumper'
-    },
-    size: {
-      type: Number,
-      default: 10
     },
     total: {
       type: Number,
       default: 100
     }
   })
-  const emits = defineEmits(['change', 'update:current', 'update:size'])
-
-  const current = ref(props.current)
-  const size = ref(props.size)
-  /**
-   * 当前页码或者页数大小改变时的回调
-   * 调用自定义函数currentChange
-   */
-  const currentChange = () => {
-    emits('update:size', size.value)
-    emits('update:current', current.value)
+  const emits = defineEmits(['change'])
+  const sizeChangeHandler = () => {
+    current.value = 1
     emits('change')
   }
-  watch(
-    () => props.current,
-    () => {
-      current.value = props.current
-      emits('change')
-    }
-  )
-  watch(
-    () => props.size,
-    () => {
-      size.value = props.size
-      emits('change')
-    }
-  )
 </script>
