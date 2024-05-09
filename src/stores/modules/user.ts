ddb101fa6d5a8015e7ca2490aa84b9d943cc73d3
lichang <2798323033>
2024-05-09 15:33:44 +0800

    update:使用useLocalStorage替换工具函数

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -3,19 +3,17 @@ import { type UserType } from '#/userinfo'
 import { LocalStorageKeys } from '@/enums/localstorage'
 import { getItem, setItem, removeItem } from '@/utils'
 import { defineStore } from 'pinia'
-
+import { useLocalStorage } from '@/hooks'
 interface LoginRequestData {
   username: string
   password: string
 }
 
 export const useUserStore = defineStore('user', () => {
-  const userinfo = ref<UserType | null>(getItem(LocalStorageKeys.USERINFO))
+  const userinfo = useLocalStorage<UserType | null>(LocalStorageKeys.USERINFO)
   /** 登录操作 */
   const login = async ({ username, password }: LoginRequestData) => {
-    console.log('用户名:' + username, '密码:' + password)
-    setItem(LocalStorageKeys.USERINFO, { username, password, user_type: 'admin' })
-    userinfo.value = getItem(LocalStorageKeys.USERINFO)
+    userinfo.value = { username, password, user_type: 'admin' }
   }
   /** 退出登录操作 */
   const logout = async () => {
