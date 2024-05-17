2bbcd8516c6bc84ef2a2115526081c51a0efecf8
lichang <dotb116393@163.com>
2024-05-17 14:33:46 +0800

    del:删除无用代码

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,48 +1,4 @@
 /**
- * 对数组排序，不改变原数组
- * @param {Array} arr 目标数组
- * @param {string} valueName 对象数组中的某个值进行排序;填了此项时，flag必填
- * @param {boolean} flag 不填或者true是从升序；false为降序;
- * @returns 返回排序后的新数组；flase:数组为空不是数组
- * @example 对this.arr中每个对象的time属性进行降序排序
- * const newArr = arrSort(oldArr,false,'time')
- * @author 李畅
- * @email dotb116393@163.com
- */
-export const arrSort = <T>(arr: Array<T>, valueName: string, flag: boolean): Array<T> | boolean => {
-  if (flag === undefined) {
-    flag = true
-  }
-  // 判断是否是数组
-  if (isArray(arr)) {
-    // 判断数组长度是否为0
-    if (arr.length !== 0) {
-      let newArr = []
-      newArr = JSON.parse(JSON.stringify(arr)) as Array<T>
-      // 对属性排序
-      if (valueName) {
-        if (flag) {
-          newArr.sort((a: any, b: any) => Date.parse(a[valueName]) - Date.parse(b[valueName]))
-        } else {
-          newArr.sort((a: any, b: any) => Date.parse(b[valueName]) - Date.parse(a[valueName]))
-        }
-      } else {
-        if (flag) {
-          newArr.sort((a: any, b: any) => a - b)
-        } else {
-          newArr.sort((a: any, b: any) => b - a)
-        }
-      }
-      return newArr
-    } else {
-      return false
-    }
-  } else {
-    return false
-  }
-}
-
-/**
  * 是否是数组
  * @param target 目标
  * @returns true||false
--- src/views/sys/documents/utils/components/ArrayUse.vue
+++ src/views/sys/documents/utils/components/ArrayUse.vue
@@ -22,44 +22,6 @@
         </div>
       </div>
     </el-card>
-    <!-- 对数组排序(arrSort) -->
-    <el-card>
-      <div class="fs-base font-semibold text-red-700">
-        对数组排序(arrSort)
-        <el-icon class="cursor-pointer" @click="copyUtils('arrSort')">
-          <DocumentCopy />
-        </el-icon>
-      </div>
-      <div class="mt-2.5">
-        对数组排序，不改变原数组，三个参数
-        <br />
-        参数1:目标数组
-        <br />
-        参数2:不填或者true是从升序；false为降序;
-        <br />
-        参数3:对象数组中的某个值进行排序;填了此项时，flag必填
-        <br />
-      </div>
-      <div class="mt-2.5 pl-1 pt-1 pr-1 pb-1">
-        <div>
-          <span class="text-orange-500">arrSort</span>
-          <span class="text-sky-600">([0,4,2,3,6])</span>
-          -> [0,2,3,4,6]
-        </div>
-        <div class="mt-2.5">
-          <span class="text-orange-500">arrSort</span>
-          <span class="text-sky-600">([0,4,2,3,6])</span>
-          -> [6,4,3,2,0]
-        </div>
-        <div class="mt-2.5">
-          <span class="text-orange-500">arrSort</span>
-          <span class="text-sky-600">
-            ([{ time:2 , age:5 },{ time:5 , age:1 },{ time:1 , age:3 }, false , 'age' ])
-          </span>
-          -> [{ time:2 , age:5 },{ time:1 , age:3},{ time:5 , age:1 }]
-        </div>
-      </div>
-    </el-card>
     <!-- 返回一个被full填充的长度为length的数组(arrFull) -->
     <el-card>
       <div class="fs-base font-semibold text-red-700">
@@ -87,7 +49,6 @@
         </div>
       </div>
     </el-card>
-    <!-- 对数组排序(arrSort) -->
   </div>
 </template>
 
