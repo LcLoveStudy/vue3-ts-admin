a9cabaa44f89ff9017f41f23aeea566d1bf8804d
lichang <dotb116393@163.com>
2024-05-28 13:14:07 +0800

    fix:修复useLocalStorage的bug

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -6,14 +6,18 @@ import type { LocalStorageKeys } from '@/enums/localstorage'
  * @param key
  * @returns 返回一个ref对象，修改该对象会同步到localstorage中，赋值为null时清空该localstorage
  */
-export const useLocalStorage = <T>(key: LocalStorageKeys): Ref<T> => {
-  const storageItem = ref(
-    JSON.parse(localStorage.getItem(key + ConstEnums.PROJECT_NAME) || JSON.stringify(''))
-  )
+export const useLocalStorage = <T>(key: LocalStorageKeys, defaultValue?: T): Ref<T | undefined> => {
+  const storageItem = ref<T>()
+  const initValue = localStorage.getItem(key + ConstEnums.PROJECT_NAME)
+  if (initValue === null && defaultValue) {
+    storageItem.value = defaultValue
+  } else if (initValue) {
+    storageItem.value = JSON.parse(initValue)
+  }
   watch(
     () => storageItem.value,
     (newValue) => {
-      if (newValue === null) {
+      if (newValue === null || newValue === undefined) {
         localStorage.removeItem(key + ConstEnums.PROJECT_NAME)
       } else {
         localStorage.setItem(key + ConstEnums.PROJECT_NAME, JSON.stringify(newValue))
