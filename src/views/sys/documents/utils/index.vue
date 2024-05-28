7539897a0c7797b04f7875efdfc80cb15a484ff4
lichang <dotb116393@163.com>
2024-05-28 14:49:56 +0800

    del:删除无用代码

--- src/views/sys/documents/utils/index.vue
+++ src/views/sys/documents/utils/index.vue
@@ -1,12 +1,4 @@
 <template>
-  <el-card class="mb-5 text-2xl">
-    <span class="text-red-700">vueuse:一款非常好用官方工具库</span>
-    <br />
-    <div class="flex items-center mt-2 text-base">
-      更多配置查看
-      <el-link type="primary" @click="goVueUse"><span class="text-base">官网</span></el-link>
-    </div>
-  </el-card>
   <el-tabs tab-position="left">
     <el-tab-pane label="通用">
       <BaseUse />
