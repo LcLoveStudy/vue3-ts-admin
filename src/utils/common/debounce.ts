1c54cb8309f3d115918ff88f11c580e014d1eef0
lichang <dotb116393@163.com>
2024-05-31 17:19:07 +0800

    fix:更新防抖函数

--- src/utils/common/debounce.ts
+++ src/utils/common/debounce.ts
@@ -1,24 +1,15 @@
 /**
  * 防抖函数,在相应的时间后会执行一次,如果再次触发会重新计时
- * @param cb 回调函数
+ * @param fn 回调函数
  * @param delay 延迟的时间
- * @param immediate 是否立刻执行
  * @returns
  */
-export const debounce = (cb: Function, delay: number, immediate: boolean = false) => {
-  let timer: number = 0 // timer计算器
-  let runNow = true // 是否立刻执行一次
-  return function () {
+export const debounce = (fn: Function, delay = 500) => {
+  let timer: number | null = null
+  return () => {
     if (timer) {
       clearTimeout(timer)
     }
-    if (immediate && runNow) {
-      // 如果立刻执行,执行完一次后将runNow改为false则不会再次立刻调用
-      cb()
-      timer = setTimeout(cb, delay)
-      runNow = false
-    } else {
-      timer = setTimeout(cb, delay)
-    }
+    timer = setTimeout(fn, delay)
   }
 }
