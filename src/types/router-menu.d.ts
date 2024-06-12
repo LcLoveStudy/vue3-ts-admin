f77ec3f4fdd955fdca843cb0ef170c28ff781219
lichang <dotb116393@163.com>
2024-06-12 13:30:49 +0800

    :book:添加注释

--- src/types/router-menu.d.ts
+++ src/types/router-menu.d.ts
@@ -1,3 +1,15 @@
+/**
+ * meta:{
+ *  hideChildrenInMenu:是否在菜单中隐藏子项
+ *  orderNo:在菜单中的第几项
+ *  icon:图标
+ *  role:权限
+ *  title:菜单上显示的名称
+ *  hideMenu:是否在菜单上隐藏
+ *  hideBreadcrumb:是否在面包屑上隐藏
+ *  keepChild:是否是嵌套路由
+ * }
+ */
 declare type RouterMetaType = {
   hideChildrenInMenu: boolean
   orderNo?: number
@@ -14,15 +26,6 @@ declare type RouterMetaType = {
  * path:路径(如果有子路由需要和子路由第一项保持一致)
  * name:名称(要和路径最后一级保持一致)
  * component:要渲染的组件
- * meta:{
- *  hideChildrenInMenu:是否在菜单中隐藏子项
- *  orderNo:在菜单中的第几项
- *  icon：图标
- *  title：菜单上显示的名称
- *  hideMenu:是否在菜单上隐藏
- *  hideBreadcrumb：是否在面包屑上隐藏
- *  keepChild : 是否是嵌套路由
- * }
  * children:子路由配置
  */
 declare interface RouterType {
