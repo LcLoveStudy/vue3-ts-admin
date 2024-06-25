356aa4ce4257dc8ac3203b88b4301ebffcbef4cb
lichang <dotb116393@163.com>
2024-06-25 10:26:51 +0800

    :wrench:解决element-plus类型丢失的问题

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,13 @@
 /// <reference types="vite/client" />
+// 解决element-plus类型丢失的问题
+/// <reference types="element-plus/global.d.ts" />
+// 解决element-plus国际化的报错
+declare module 'element-plus/dist/locale/zh-cn.mjs'
 declare module '*.vue' {
   // 解决引入组件报错的问题
   import { ComponentOptions } from 'vue'
   const componentOptions: ComponentOptions
   export default componentOptions
 }
-// 解决element-plus国际化的报错
-declare module 'element-plus/dist/locale/zh-cn.mjs'
 declare let Tiff
 declare module 'leaflet-minimap'
