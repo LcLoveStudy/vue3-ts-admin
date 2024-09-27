36659c9cb2a7d793a6178660b41499f3d56aa02a
LcLoveStudy <LcLoveStudy@users.noreply.github.com>
2024-09-27 09:04:42 +0000

    deploy: d92caa3328f5b2161435428697c9702c1930fc86

---
+++ static/js/index-0aa85f73.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-4ebb649a.js";import{ai as t,E as _,G as c,U as n}from"./vue-7add97c1.js";import"./elementPlus-34158748.js";import"./elementIcons-e0fb6cdd.js";const r={},s={class:"main"};function a(i,d){const e=t("router-view");return _(),c("div",s,[n(e)])}const u=o(r,[["render",a],["__scopeId","data-v-a085ec3c"]]);export{u as default};
