494b960bb458478caea3f0fe0eb5e46bd56b25a4
lichang <dotb116393@163.com>
2025-01-01 23:08:30 +0800

    :art:refactor folder

--- src/views/login/components/form-side/components/login-form.vue
+++ src/views/login/components/form-side/components/login-form.vue
@@ -1,43 +1,28 @@
 <template>
-  <div class="bg-[#16181c] text-white tablet:px-12 phone:px-[5vw]">
-    <div class="flex items-center gap-4 tablet:mt-16 phone:mt-[4vh] select-none">
-      <h1 class="tablet:text-5xl phone:text-[24px]">欢迎回来</h1>
-      <img class="tablet:w-16 phone:w-[36px]" :src="DogIcon" />
-    </div>
-    <div class="mt-6 tablet:text-xl phone:text-[14px] text-slate-200 select-none">
-      输入您的账号密码登录系统
-    </div>
-    <el-form
-      ref="loginFormRef"
-      :model="loginForm"
-      :rules="rules"
-      label-width="auto"
-      class="tablet:mt-8 phone:mt-[3vh]"
-    >
-      <el-form-item prop="username">
-        <el-input v-model="loginForm.username" placeholder="请输入用户名" />
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
-          placeholder="请输入密码"
-          show-password
-          class="mt-8"
-        />
-      </el-form-item>
-      <el-form-item prop="status">
-        <slider-verify class="mt-8" v-model:status="loginForm.status" width="100%" />
-      </el-form-item>
-    </el-form>
-    <el-button @click="loginHandler" :loading="loginLoading" class="w-full mt-8" type="primary">
-      登录
-    </el-button>
-  </div>
+  <el-form
+    ref="loginFormRef"
+    :model="loginForm"
+    :rules="rules"
+    label-width="auto"
+    class="tablet:mt-8 phone:mt-[3vh]"
+  >
+    <el-form-item prop="username">
+      <el-input v-model="loginForm.username" placeholder="请输入用户名" />
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input v-model="loginForm.password" placeholder="请输入密码" show-password class="mt-8" />
+    </el-form-item>
+    <el-form-item prop="status">
+      <slider-verify class="mt-8" v-model:status="loginForm.status" width="100%" />
+    </el-form-item>
+  </el-form>
+  <el-button @click="loginHandler" :loading="loginLoading" class="w-full mt-8" type="primary">
+    登录
+  </el-button>
 </template>
 
 <script setup lang="ts">
   import SliderVerify from '@/components/slider-verify'
-  import DogIcon from '@/assets/images/login/dog.png'
   import { useUserStore } from '@/stores/modules/user'
   import type { FormInstance, FormRules } from 'element-plus'
   const router = useRouter()
@@ -50,7 +35,6 @@
   })
 
   const loginLoading = ref(false)
-
   const rules = reactive<FormRules>({
     username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
     password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
---
+++ src/views/login/components/form-side/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-[#16181c] text-white tablet:px-12 phone:px-[5vw]">
+    <div class="flex items-center gap-4 tablet:mt-16 phone:mt-[4vh] select-none">
+      <h1 class="tablet:text-5xl phone:text-[24px]">欢迎回来</h1>
+      <img class="tablet:w-16 phone:w-[36px]" :src="DogIcon" />
+    </div>
+    <div class="mt-6 tablet:text-xl phone:text-[14px] text-slate-200 select-none">
+      输入您的账号密码登录系统
+    </div>
+    <login-form />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import DogIcon from '@/assets/images/login/dog.png'
+  import { LoginForm } from './components'
+</script>
