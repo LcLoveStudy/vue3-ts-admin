647bd54edacb7157cbe05456919af217d3442e57
lichang <dotb116393@163.com>
2025-01-09 16:36:41 +0800

    :lipstick: update border

--- src/layout/sys-layout/index.vue
+++ src/layout/sys-layout/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="w-full h-full flex">
     <div
-      class="h-full desktop:p-2 duration-[380ms] overflow-hidden bg-white dark:bg-dark-primary"
+      class="h-full desktop:p-2 duration-[380ms] border-0 border-r-[1px] border-solid border-[#eee] dark:border-[#434343] overflow-hidden bg-white dark:bg-dark-primary"
       :class="[asideMenuFold ? 'w-[5vw]' : 'w-[15vw]']"
     >
       <slot name="aside"></slot>
     </div>
     <div class="h-full duration-[380ms]" :class="[asideMenuFold ? 'w-[95vw]' : 'w-[85vw]']">
-      <div class="h-[7vh] bg-white dark:bg-dark-primary">
+      <div
+        class="h-[7vh] bg-white dark:bg-dark-primary border-0 border-b-[1px] border-solid border-[#eee] dark:border-[#434343]"
+      >
         <slot name="header"></slot>
       </div>
       <div class="desktop:p-6 bg-[--container-main-bgc] h-[93vh]">
