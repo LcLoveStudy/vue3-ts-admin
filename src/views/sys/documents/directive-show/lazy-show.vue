481b0a90ca5433d13d55501d72e9cb8ac99b4e5c
lichang <dotb116393@163.com>
2025-04-08 15:24:01 +0800

    :lipstick: update style

--- src/views/sys/documents/directive-show/lazy-show.vue
+++ src/views/sys/documents/directive-show/lazy-show.vue
@@ -31,37 +31,37 @@
     class="page_main phone:h-[40vh] tablet:h-[80vh] phone:mt-[3vh] tablet:top-[10vh] tablet:right-[1vw] phone:w-[90vw] tablet:w-[40vw]"
   >
     <img
-      class="phone:h-[20vh] tablet:h-[60v]"
+      class="phone:h-[20vh] tablet:h-[60v] desktop:h-[50vh]"
       v-lazy="
         'https://img2.baidu.com/it/u=2098302066,1207295454&fm=253&fmt=auto&app=138&f=JPEG?w=562&h=343'
       "
     />
     <img
-      class="phone:h-[20vh] tablet:h-[60v]"
+      class="phone:h-[20vh] tablet:h-[60v] desktop:h-[50vh]"
       v-lazy="
         'https://img1.baidu.com/it/u=2676533820,1803958359&fm=253&fmt=auto&app=138&f=JPEG?w=600&h=400'
       "
     />
     <img
-      class="phone:h-[20vh] tablet:h-[60v]"
+      class="phone:h-[20vh] tablet:h-[60v] desktop:h-[50vh]"
       v-lazy="
         'https://img2.baidu.com/it/u=3196968309,1319515521&fm=253&fmt=auto&app=138&f=PNG?w=751&h=500'
       "
     />
     <img
-      class="phone:h-[20vh] tablet:h-[60v]"
+      class="phone:h-[20vh] tablet:h-[60v] desktop:h-[50vh]"
       v-lazy="
         'https://img2.bianews.com/image/bianews/news/imgs/1632407012152.jpeg?imageView2/0/format/jpg/q/75'
       "
     />
     <img
-      class="phone:h-[20vh] tablet:h-[60v]"
+      class="phone:h-[20vh] tablet:h-[60v] desktop:h-[50vh]"
       v-lazy="
         'https://img0.baidu.com/it/u=3664273503,2975533171&fm=253&fmt=auto&app=138&f=JPEG?w=650&h=298'
       "
     />
     <img
-      class="phone:h-[20vh] tablet:h-[60v]"
+      class="phone:h-[20vh] tablet:h-[60v] desktop:h-[50vh]"
       v-lazy="'https://p6.itc.cn/images01/20200917/7656d7a7ea3f4aeaa97d302b94a5190e.png'"
     />
   </el-card>
