138b35cbb880a1a77e0c9cd204c598cf64039b2c
lichang <dotb116393@163.com>
2025-04-30 14:07:45 +0800

    :art: optimize the code

--- src/views/sys/components/aside-menu-fold-btn/index.vue
+++ src/views/sys/components/aside-menu-fold-btn/index.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="fold_icon tablet:px-2 rounded" :class="[asideMenuFold ? '' : 'tablet:ml-4']">
-    <el-icon class="text-black dark:text-white" @click="asideMenuFold = !asideMenuFold">
-      <Expand v-if="asideMenuFold" />
-      <Fold v-else />
-    </el-icon>
-  </div>
+  <el-button
+    @click="asideMenuFold = !asideMenuFold"
+    :class="[asideMenuFold ? '' : 'tablet:ml-4']"
+    class="tablet:px-2"
+    type="primary"
+    text
+    bg
+    :icon="asideMenuFold ? Expand : Fold"
+  ></el-button>
 </template>
 
 <script setup lang="ts">
+  import { Expand, Fold } from '@element-plus/icons-vue'
   import { storeToRefs } from 'pinia'
   import { useCustomStore } from '@/stores'
   const { asideMenuFold } = storeToRefs(useCustomStore())
 </script>
-
-<style lang="less" scoped>
-  .fold_icon {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 1.8rem;
-    cursor: pointer;
-  }
-
-  .fold_icon:hover {
-    background-color: var(--hover-backgroundColor);
-  }
-</style>
